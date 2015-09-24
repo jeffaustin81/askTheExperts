79effbd69665e922beb779cafb5a5377f2d75110
Filipe Silvestre & Jeff Austin <student@epicodus.com>
2015-09-24 16:30:57 -0700

    Add upvote and downvote feature

--- controllers/AnswersController.js
+++ controllers/AnswersController.js
@@ -2,13 +2,23 @@ questionsAnswers.controller('AnswersCtrl', function AnswersCtrl($scope, $statePa
   $scope.question = UtilitiesFactory.findById(QuestionsFactory.questions, $stateParams.questionId)
   $scope.addAnswer = function() {
     $scope.question.answers.push({ name: $scope.answerName, id: $scope.question.answers.length + 1, votes: 0 });
-    $scope.answer = UtilitiesFactory.findById(QuestionsFactory.questions.answers, $stateParams.answersId)
     $scope.answerName = null;
   };
-    $scope.upVote = function() {
-        console.log($scope.question.answers);
-    $scope.question.answer.votes += 1;
+    $scope.upVote = function($scope) {
+        // console.log($scope);
+        // console.log($scope.answer);
+        // $scope.answer = UtilitiesFactory.findById(QuestionsFactory.questions.answers, $stateParams.)
+
+    $scope.votes += 1;
   };
+
+  $scope.downVote = function($scope) {
+      // console.log($scope);
+      // console.log($scope.answer);
+      // $scope.answer = UtilitiesFactory.findById(QuestionsFactory.questions.answers, $stateParams.)
+
+  $scope.votes -= 1;
+};
 });
 
   // var answer = { votes: 0 };
