d09de34710feb8e4f38fa90365bbb3a5af3cff7b
Jeff Austin <jeffaustin81@gmail.com>
2015-09-24 20:43:33 -0700

    Delete unecessary variable

--- controllers/AnswersController.js
+++ controllers/AnswersController.js
@@ -3,10 +3,7 @@ questionsAnswers.controller('AnswersCtrl', function AnswersCtrl($scope, $statePa
   $scope.addAnswer = function() {
     $scope.question.answers.push({ name: $scope.answerName, id: $scope.question.answers.length + 1, votes: 0 });
     $scope.answerName = null;
-
-    $scope.answer = UtilitiesFactory.findById(QuestionsFactory.questions.answers, $stateParams.answerId)
-  };
-
+};
     $scope.upVote = function($scope) {
         // console.log($scope);
         // console.log($scope.answer);
