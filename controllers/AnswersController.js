b92875cd54255000bb7ce8ce8fd4cbde2d0cbcff
Jeff Austin <jeffaustin81@gmail.com>
2015-09-24 20:33:43 -0700

    Commit

--- controllers/AnswersController.js
+++ controllers/AnswersController.js
@@ -2,12 +2,13 @@ questionsAnswers.controller('AnswersCtrl', function AnswersCtrl($scope, $statePa
   $scope.question = UtilitiesFactory.findById(QuestionsFactory.questions, $stateParams.questionId)
   $scope.addAnswer = function() {
     $scope.question.answers.push({ name: $scope.answerName, id: $scope.question.answers.length + 1, votes: 0 });
-    $scope.answer = UtilitiesFactory.findById(QuestionsFactory.questions.answers, $stateParams.answersId)
     $scope.answerName = null;
+
+    $scope.answer = UtilitiesFactory.findById(QuestionsFactory.questions.answers, $stateParams.answerId)
   };
     $scope.upVote = function() {
         console.log($scope.question.answers);
-    $scope.question.answer.votes += 1;
+    $scope.question.answer.votes + 1;
   };
 });
 
