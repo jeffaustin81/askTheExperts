672607dd3445095b15388425ada0cfd131d3a9b0
Jeff Austin <jeffaustin81@gmail.com>
2015-09-24 20:45:08 -0700

    Delete console logs and reference code

--- controllers/AnswersController.js
+++ controllers/AnswersController.js
@@ -5,28 +5,13 @@ questionsAnswers.controller('AnswersCtrl', function AnswersCtrl($scope, $statePa
     $scope.answerName = null;
 };
     $scope.upVote = function($scope) {
-        // console.log($scope);
-        // console.log($scope.answer);
-        // $scope.answer = UtilitiesFactory.findById(QuestionsFactory.questions.answers, $stateParams.)
 
     $scope.votes += 1;
   };
 
   $scope.downVote = function($scope) {
-      // console.log($scope);
-      // console.log($scope.answer);
-      // $scope.answer = UtilitiesFactory.findById(QuestionsFactory.questions.answers, $stateParams.)
 
   $scope.votes -= 1;
 };
 });
 
-  // var answer = { votes: 0 };
-  // $scope.addAnswer = function() {
-  //   answer.name = $scope.answerName;
-  //   $scope.question.answers.push(answer);
-  //   $scope.answerName = null;
-  // };
-  //   $scope.upVote = function() {
-  //   answer.votes += 1;
-  // };
